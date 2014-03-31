d4eeb354b5de258dd3b7f9cdf7871792cf7398de
Thomas Dufour <td@thomasdufour.fr>
2014-03-31 22:54:07 +0200

    [config] Basic types, decided on yaml-light as format lib

--- src/Hackup/Config.hs
+++ src/Hackup/Config.hs
@@ -1,8 +1,35 @@
-module Hackup.Config where
+{-# LANGUAGE TemplateHaskell #-}
+
+module Hackup.Config (Config, readConfig) where
 
 import Hackup.Types
+import Control.Lens
 
-data Config = Config {} deriving (Show, Eq)
+data Config = Config { _backupRootDir :: FilePath
+                     , _defaultKeep :: Integer
+                     , _sections :: [Section] 
+                     } deriving (Show, Eq)
+
+
+data Section = Section { _name :: String
+                       , _archiveName :: Maybe String
+                       , _archiveDir :: Maybe FilePath
+                       , _keep :: Maybe Integer
+                       , _items :: [FileSelector]
+                       , _before :: [Command]
+                       , _after :: [Command]
+                       } deriving (Show, Eq)
+                       
+data Command = Command { _command :: String
+                       , _workingDir :: Maybe FilePath
+                       , _fail :: Bool 
+                       } deriving (Show, Eq)
+
+data FileSelector = Glob String | Regex String deriving (Show, Eq)
+
+makeLenses ''Config
+makeLenses ''Section
+makeLenses ''Command                       
 
 readConfig :: FilePath -> TryIO Config
 readConfig _ = failWith "Failed explicilty at readConfig"
