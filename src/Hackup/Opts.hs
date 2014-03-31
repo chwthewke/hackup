ad740842df70a386b6b7c32ec5cec351c8711b96
Thomas Dufour <tdufour@ankama.com>
2014-03-31 18:09:58 +0200

    [cmd-opts] Command-line options parsing (in progress)

--- src/Hackup/Opts.hs
+++ src/Hackup/Opts.hs
@@ -1,9 +1,38 @@
-module Hackup.Opts where
+module Hackup.Opts (Opts, cmdOpts, configFile, dryRun) where
 
 import Hackup.Types
+import Options.Applicative
+import Options.Applicative.Extra
+import System.Environment
 
 data Opts = Opts { configFile :: String
+                 , dryRun :: Bool
+--                 , quiet :: Bool
                  } deriving (Show, Eq)
+
+programName :: String
+programName = "hackup"
+
+parser :: Parser Opts
+parser = Opts
+  <$> strOption (
+    long "config-file"
+      <> short 'f'
+      <> metavar "file"
+      <> help "required, the configuration file to read.")
+  <*> switch (
+    long "dry-run"
+      <> short 'n'
+      <> help "don't run the backup, just show what would happen.")
+
+parserInfo :: ParserInfo Opts
+parserInfo = info (helper <*> parser) (
+  fullDesc
+    <> progDesc "Create configurable backups."
+    <> header "hackup 0.1")
+
+co :: IO (Maybe Opts)
+co = getArgs >>= return . execParserMaybe parserInfo
 
 cmdOpts :: TryT IO Opts
 cmdOpts = undefined
