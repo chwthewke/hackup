e30d9d2562cfc8ad9b940469fb82fdd41382bb4c
Thomas Dufour <tdufour@ankama.com>
2014-04-01 18:06:21 +0200

    [config] Progress in Config parser

--- src/Hackup/Config.hs
+++ src/Hackup/Config.hs
@@ -5,6 +5,9 @@ module Hackup.Config (Config, readConfig) where
 
 import Hackup.Types
 import Control.Lens
+import Control.Monad
+import Control.Applicative
+import Data.Maybe (mapMaybe)
 import Data.Yaml
 import Data.Text
 
@@ -34,15 +37,39 @@ makeLenses ''Config
 makeLenses ''Section
 makeLenses ''Command
 
+extractSelectorType :: Text -> FileSelector
+extractSelectorType fs = Prelude.head $ mapMaybe matchSelector [ ("glob:", Glob), ("regex:", Regex), ("", Glob) ]  
+  where matchSelector (s, st) 
+          | s `isPrefixOf` fs = Just . st . unpack $ Data.Text.drop (Data.Text.length s) fs
+          | otherwise         = Nothing
+
 instance FromJSON FileSelector where
-  parseJSON (String s)
-    | "glob:" `isPrefixOf` s =
-      return . Glob $ unpack (Data.Text.drop 5 s)
-    | "regex:" `isPrefixOf` s =
-      return . Regex $ unpack (Data.Text.drop 6 s)
-    | otherwise = return . Glob $ unpack s
---  parseJSON _ = mzero
-  
+  parseJSON (String s) = return $ extractSelectorType s
+  parseJSON _ = mzero
+
+instance FromJSON Command where
+  parseJSON (Object v) = Command <$>
+                           v .: "command" <*>
+                           v .:? "workingDir" <*>
+                           v .:? "fail" .!= True
+  parseJSON _          = mzero
+
+sectionFromJSON :: Text -> Value -> Parser Section
+sectionFromJSON name (Object v) = Section (unpack name) <$>
+                                    v .:? "archive" <*>
+                                    v .:? "targetDir" <*>
+                                    v .:? "keep" <*>
+                                    v .: "contents" <*>
+                                    v .:? "before" .!= [] <*>
+                                    v .:? "after" .!= []
+
+instance FromJSON Config where
+  parseJSON (Object v) = Config <$>
+                           v .: "rootDir" <*>
+                           v .:? "keep" .!= 7 <*>
+                           undefined 
+  parseJSON _          = mzero
+
 readConfig :: FilePath -> TryIO Config
 readConfig _ = failWith "Failed explicilty at readConfig"
 
