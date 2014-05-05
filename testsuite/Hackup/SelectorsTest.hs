ebeb60a7a5fba3659944a68d940549184e0234a0
Thomas Dufour <td@thomasdufour.fr>
2014-05-05 15:03:44 +0200

    [selectors] Fix tests in Linux by guarding windows specific assertions

--- testsuite/Hackup/SelectorsTest.hs
+++ testsuite/Hackup/SelectorsTest.hs
@@ -2,9 +2,10 @@
 
 module Hackup.SelectorsTest where
 
-import           Distribution.System     (OS (..), buildOS)
+import           Control.Monad           (when)
 import           System.Directory.Layout
 import           System.FilePath
+import           System.Info             (os)
 import           System.IO.Temp
 import           Test.Framework
 
@@ -21,6 +22,13 @@ testLayout1 = do
           file_ "four.jpg"
         file_ "five.txt"
 
+isWindows :: Bool
+isWindows = isWindows' os
+  where isWindows' "win32" = True
+        isWindows' "migw32" = True
+        isWindows' "cygwin32" = True
+        isWindows' _ = False
+
 exerciseSelector :: (FilePath -> IO [FilePath]) -> Layout -> ([FilePath] -> IO r) -> IO r
 exerciseSelector sel layout f =
   withSystemTempDirectory "testtmp" (\ base -> make layout base >> sel base >>= f)
@@ -35,7 +43,7 @@ test_globSelectsSubDir :: IO ()
 test_globSelectsSubDir = testGlob "a/**" testLayout1 [ "a" </> "one.txt", "a" </> "two.jpg" ]
 
 test_globSelectsSubDirWithBackslash :: IO ()
-test_globSelectsSubDirWithBackslash = testGlob "a\\**" testLayout1 [ "a" </> "one.txt", "a" </> "two.jpg" ]
+test_globSelectsSubDirWithBackslash = when isWindows $ testGlob "a\\**" testLayout1 [ "a" </> "one.txt", "a" </> "two.jpg" ]
 
 test_globSelectsByExtension :: IO ()
 test_globSelectsByExtension = testGlob "**/*.txt" testLayout1 [ "a" </> "one.txt", "b" </> "d" </> "three.txt", "five.txt" ]
@@ -45,13 +53,12 @@ test_globSelectsEmptyDirectories = testGlob "b/**/*" testLayout1 [ "b" </> "c", 
 
 -- regex are non-portable, we'll have to have conditional tests
 
-data PortableRegex = PortableRegex { win :: String
+data PortableRegex = PortableRegex { win   :: String
                                    , posix :: String
                                    } deriving (Show, Eq)
 
 testRegex :: PortableRegex -> Layout -> [FilePath] -> IO ()
-testRegex pr = case buildOS of Windows -> testSelector Regex (win pr)
-                               _       -> testSelector Regex (posix pr)
+testRegex = testSelector Regex . (if isWindows then win else posix)
 
 test_regexSelectsSubdirContents :: IO ()
 test_regexSelectsSubdirContents = testRegex (PortableRegex "a\\\\.*" "a/.*") testLayout1 [ "a" </> "one.txt", "a" </> "two.jpg" ]
